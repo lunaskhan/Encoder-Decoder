9719d8561be637fe881b066644de9d769bf9d331
unknown <lunakhan@ufl.edu>
2023-03-23 23:49:58 -0400

    added decoder and main func

--- main.py
+++ main.py
@@ -6,9 +6,28 @@ def encode(password):
     return encoded
 
 
+def decode(password):
+    decoded = ''
+    for i in password:
+        c = (int(i) - 3) % 10
+        decoded += str(c)
+    return decoded
+
+
 def main():
-    password = input("Enter a password to encode: ")
-    print(encode(password))
+    password = ''
+    while True:
+        print('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit')
+        op = input('Please enter an option: ')
+        if op == '1':
+            password = encode(input("Enter your password to encode: "))
+            print('Your password has been encoded and stored!')
+        elif op == '2':
+
+            print(f'The encoded password is {password}, and the original password is {decode(password)}.')
+        elif op == '3':
+            exit()
+
 
 if __name__ == '__main__':
     main()
